e90655933acdebc4d548fc205f6b88475f504a29
Acedia / Melancholy <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2023-02-10 17:30:34 +0630

    Add conformance tests and add old tests to staging
    Conformance tests need to be redone to remove any tests that rely on stack depth and very specific behaviours we cannot emulate.

--- bin/OutputBytes.lua
+++
@@ -1,5 +0,0 @@
-local f1 = io.open("output.luac", "rb");
-local bytecode = f1:read("*all");
-f1:close();
-
-print(bytecode:gsub(".", function(b) return "\\" .. b:byte() end))
--- bin/RunTests.lua
+++ bin/RunTests.lua
@@ -1,39 +1,138 @@
-local fiu = require("../Source")
-local allTests = {
-	"AndOr", "AndOrK", "Arit", "AritK", "Booleans", "Concat", "DupTableSetList", "GetImportSpecial", "Globals", "HelloWorld",
-	"LOADN", "LOADNIL", "Tables", "Unary", "Conditions", "WhileRepeat", "BasicJump", "Closure", "Namecall", "Varargs", "GenericFor",
-	"ForLoops"
+local TEST_ALL = false;
+local TEST_CONFORMANCE = false;
+local TEST_STAGING = false;
+local TEST_SPECIFIC = true;
 
-	-- Optional Tests: "GetImportSpecial", "WhileTrueDo"
-}
+local TEST_IF_OK = false;
+
+local fiu = require("../Source");
+
+local conformanceTests = {
+	"assert",
+	"attrib",
+	"basic",
+	"bitwise",
+	"calls",
+	"clear",
+	"constructs",
+	"coroutine",
+	"datetime",
+	"ifelseexpr",
+	"iter",
+	"literals",
+	"locals",
+	"math",
+	"move",
+	"pm",
+	"sort",
+	"strconv",
+	"strings",
+	"tables",
+	"tpack",
+	"utf8",
+	"vararg"
+};
+
+local stagingTests = {
+	"AndOr",
+	"AndOrK",
+	"Arit",
+	"AritK",
+	"Booleans",
+	"Concat",
+	"DupTableSetList",
+	"GetImportSpecial",
+	"Globals",
+	"HelloWorld",
+	"LOADN",
+	"LOADNIL",
+	"Tables",
+	"Unary",
+	"Conditions",
+	"WhileRepeat",
+	"BasicJump",
+	"Closure",
+	"Namecall",
+	"Varargs",
+	"GenericFor",
+	"ForLoops",
+	"Returns"
+};
 
 local specificTests = {
-	"CLOSE"
-}
+	{
+		false,
+		"Returns"
+	}
+};
 
-
-for i,v in allTests do
-	print(string.format(">>>>>>>> RUNNING TEST: %s <<<<<<<<", v))
-	print("--------->> BYTECODE LISTING <<---------")
-	print(require("Listings/"..v)())
-	print("--------->> SOURCE OUTPUT <<---------") 
-	print("SOURCE PCALL: ", pcall(function()
-		require("SourceTests/"..v)()
-	end))
-	print("--------->> VM OUTPUT <<---------") 
-
-	local s,e = pcall(function()
-		local m = require("Tests/"..v)()
-		fiu.luau_load(m, getfenv())()
-	end)
-
-	if not s then 
-		print(">>>>>>>>>>>>>>>>>>>>>>>>>>>> Broken: " .. e)
-	end
-
-	print("VM PCALL: ", s)
-
-	print(string.format(">>>>>>>> FINISHED RUNNING TEST: %s <<<<<<<<", v))
+local function TestIfOk()
+	--// TODO: Proper CI tests
 end
 
-print("ALL TESTS PASSED.")
+local function TestOutput(directory, testNames)
+	for i, v in testNames do
+		print(string.format(">>>>>>>> RUNNING TEST: %s <<<<<<<<", v));
+		print("--------->> BYTECODE LISTING <<---------");
+		print((require(string.format("%s/Listings/%s", directory, v)))());
+		print("--------->> SOURCE OUTPUT <<---------");
+		print("SOURCE PCALL: ", pcall(function()
+			(require(string.format("%s/SourceTests/%s", directory, v)))();
+		end));
+		print("--------->> VM OUTPUT <<---------");
+		local s, e = pcall(function()
+			local m = (require(string.format("%s/Tests/%s", directory, v)))();
+			(fiu.luau_load(m, getfenv()))();
+		end);
+		if not s then
+			print(">>>>>>>>>>>>>>>>>>>>>>>>>>>> Broken: " .. e);
+		end;
+		print("VM PCALL: ", s);
+		print(string.format(">>>>>>>> FINISHED RUNNING TEST: %s <<<<<<<<", v));
+	end;
+end
+
+local function Test(directory, testNames)
+	if TEST_IF_OK then 
+		TestIfOk(directory, testNames)
+	else  
+		TestOutput(directory, testNames)
+	end
+end;
+
+if TEST_ALL then
+	Test("ConformanceTests", conformanceTests)
+	Test("StagingTests", stagingTests)
+
+	return;
+end;
+
+if TEST_CONFORMANCE then 
+	Test("ConformanceTests", conformanceTests)
+
+	return;
+end 
+
+if TEST_STAGING then 
+	Test("StagingTests", stagingTests)
+
+	return;
+end 
+
+if TEST_SPECIFIC then 
+	local specificConformanceTests = {}
+	local specificStagingTests = {}
+
+	for i,v in specificTests do 
+		if v[1] then 
+			table.insert(specificConformanceTests, v[2])
+		else 
+			table.insert(specificStagingTests, v[2])
+		end 
+	end
+
+	Test("ConformanceTests", specificConformanceTests)
+	Test("StagingTests", specificStagingTests)
+end 
+
+print("FINISHED.");

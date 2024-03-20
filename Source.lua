a329323d1fc282793733b5d197b52f0e8127e0f4
Snorlax <57375992+SnorlaxAssist@users.noreply.github.com>
2024-03-20 18:49:41 -0400

    Removed usage of getmetatable in preference of fallback general_iteration

--- Source.lua
+++ Source.lua
@@ -35,14 +35,7 @@ local bit32_extract = bit32.extract
 local ttisnumber = function(v) return type(v) == "number" end
 local ttisboolean = function(v) return type(v) == "boolean" end
 local ttisstring = function(v) return type(v) == "string" end
-local ttistable = function(v) return type(v) == "table" end
-local ttisuserdata = function(v) return type(v) == "userdata" end
 local ttisfunction = function(v) return type(v) == "function" end
-
-local hastm = function(mt, e) return ttistable(mt) and rawget(mt, e) ~= nil or false end
-
-local TM_CALL = "__call"
-local TM_ITER = "__iter"
 
 -- // opList contains information about the instruction, each instruction is defined in this format:
 -- // {OP_NAME, OP_MODE, K_MODE, HAS_AUX}
@@ -812,21 +805,13 @@ local function luau_load(module, env)
 				elseif op == 58 then --[[ FORGLOOP ]]
 					local A = inst.A
 					local aux = inst.aux
- 					local res = bit32_band(aux, 0xf);
+					local res = bit32_band(aux, 0xf);
 
 					top = A + 6
 
 					local it = stack[A]
-					local callable = false
 
-					if ttistable(it) or ttisuserdata(it) then
-						local mt = getmetatable(it)
-						if hastm(mt, TM_CALL) and not hastm(mt, TM_ITER) then
-							callable = true
-						end
-					end
-
-					if ttisfunction(it) or callable then 
+					if ttisfunction(it) then 
 						local vals = { it(stack[A + 1], stack[A + 2]) }
 						table_move(vals, 1, res, A + 3, stack)
 
@@ -837,11 +822,10 @@ local function luau_load(module, env)
 							pc += 1
 						end
 					else
-						local ok, vals = coroutine_resume(generalized_iterators[inst])
+						local ok, vals = coroutine_resume(generalized_iterators[inst], it, stack[A + 1], stack[A + 2])
 						if not ok then
 							error(vals)
 						end
-
 						if vals == LUA_GENERALIZED_TERMINATOR then 
 							generalized_iterators[inst] = nil
 							pc += 1
@@ -928,23 +912,20 @@ local function luau_load(module, env)
 					pc += 1 --// adjust for aux
 				elseif op == 76 then --[[ FORGPREP ]]
 					local iterator = stack[inst.A]
-
+					
 					if not ttisfunction(iterator) then
-						local mt = getmetatable(iterator)
-						if not hastm(mt, TM_CALL) then
-							local loopInstruction = code[pc + inst.D]
-							if generalized_iterators[loopInstruction] == nil then 
-								--// Thanks @bmcq-0 and @memcorrupt for the spoonfeed
-								local function gen_iterator()
-									for r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, r50, r51, r52, r53, r54, r55, r56, r57, r58, r59, r60, r61, r62, r63, r64, r65, r66, r67, r68, r69, r70, r71, r72, r73, r74, r75, r76, r77, r78, r79, r80, r81, r82, r83, r84, r85, r86, r87, r88, r89, r90, r91, r92, r93, r94, r95, r96, r97, r98, r99, r100, r101, r102, r103, r104, r105, r106, r107, r108, r109, r110, r111, r112, r113, r114, r115, r116, r117, r118, r119, r120, r121, r122, r123, r124, r125, r126, r127, r128, r129, r130, r131, r132, r133, r134, r135, r136, r137, r138, r139, r140, r141, r142, r143, r144, r145, r146, r147, r148, r149, r150, r151, r152, r153, r154, r155, r156, r157, r158, r159, r160, r161, r162, r163, r164, r165, r166, r167, r168, r169, r170, r171, r172, r173, r174, r175, r176, r177, r178, r179, r180, r181, r182, r183, r184, r185, r186, r187, r188, r189, r190, r191, r192, r193, r194, r195, r196, r197, r198, r199, r200 in iterator do 
-										coroutine_yield({r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, r50, r51, r52, r53, r54, r55, r56, r57, r58, r59, r60, r61, r62, r63, r64, r65, r66, r67, r68, r69, r70, r71, r72, r73, r74, r75, r76, r77, r78, r79, r80, r81, r82, r83, r84, r85, r86, r87, r88, r89, r90, r91, r92, r93, r94, r95, r96, r97, r98, r99, r100, r101, r102, r103, r104, r105, r106, r107, r108, r109, r110, r111, r112, r113, r114, r115, r116, r117, r118, r119, r120, r121, r122, r123, r124, r125, r126, r127, r128, r129, r130, r131, r132, r133, r134, r135, r136, r137, r138, r139, r140, r141, r142, r143, r144, r145, r146, r147, r148, r149, r150, r151, r152, r153, r154, r155, r156, r157, r158, r159, r160, r161, r162, r163, r164, r165, r166, r167, r168, r169, r170, r171, r172, r173, r174, r175, r176, r177, r178, r179, r180, r181, r182, r183, r184, r185, r186, r187, r188, r189, r190, r191, r192, r193, r194, r195, r196, r197, r198, r199, r200})
-									end
-	
-									coroutine_yield(LUA_GENERALIZED_TERMINATOR)
+						local loopInstruction = code[pc + inst.D]
+						if generalized_iterators[loopInstruction] == nil then 
+							--// Thanks @bmcq-0 and @memcorrupt for the spoonfeed
+							local function gen_iterator(...)
+								for r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, r50, r51, r52, r53, r54, r55, r56, r57, r58, r59, r60, r61, r62, r63, r64, r65, r66, r67, r68, r69, r70, r71, r72, r73, r74, r75, r76, r77, r78, r79, r80, r81, r82, r83, r84, r85, r86, r87, r88, r89, r90, r91, r92, r93, r94, r95, r96, r97, r98, r99, r100, r101, r102, r103, r104, r105, r106, r107, r108, r109, r110, r111, r112, r113, r114, r115, r116, r117, r118, r119, r120, r121, r122, r123, r124, r125, r126, r127, r128, r129, r130, r131, r132, r133, r134, r135, r136, r137, r138, r139, r140, r141, r142, r143, r144, r145, r146, r147, r148, r149, r150, r151, r152, r153, r154, r155, r156, r157, r158, r159, r160, r161, r162, r163, r164, r165, r166, r167, r168, r169, r170, r171, r172, r173, r174, r175, r176, r177, r178, r179, r180, r181, r182, r183, r184, r185, r186, r187, r188, r189, r190, r191, r192, r193, r194, r195, r196, r197, r198, r199, r200 in ... do 
+									coroutine_yield({r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25, r26, r27, r28, r29, r30, r31, r32, r33, r34, r35, r36, r37, r38, r39, r40, r41, r42, r43, r44, r45, r46, r47, r48, r49, r50, r51, r52, r53, r54, r55, r56, r57, r58, r59, r60, r61, r62, r63, r64, r65, r66, r67, r68, r69, r70, r71, r72, r73, r74, r75, r76, r77, r78, r79, r80, r81, r82, r83, r84, r85, r86, r87, r88, r89, r90, r91, r92, r93, r94, r95, r96, r97, r98, r99, r100, r101, r102, r103, r104, r105, r106, r107, r108, r109, r110, r111, r112, r113, r114, r115, r116, r117, r118, r119, r120, r121, r122, r123, r124, r125, r126, r127, r128, r129, r130, r131, r132, r133, r134, r135, r136, r137, r138, r139, r140, r141, r142, r143, r144, r145, r146, r147, r148, r149, r150, r151, r152, r153, r154, r155, r156, r157, r158, r159, r160, r161, r162, r163, r164, r165, r166, r167, r168, r169, r170, r171, r172, r173, r174, r175, r176, r177, r178, r179, r180, r181, r182, r183, r184, r185, r186, r187, r188, r189, r190, r191, r192, r193, r194, r195, r196, r197, r198, r199, r200})
 								end
-	
-								generalized_iterators[loopInstruction] = coroutine_create(gen_iterator)
+
+								coroutine_yield(LUA_GENERALIZED_TERMINATOR)
 							end
+
+							generalized_iterators[loopInstruction] = coroutine_create(gen_iterator)
 						end
 					end
 

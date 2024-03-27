88ef90f5b174e55760aec87d9c9efb3c83b3c3b0
James <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2024-03-27 20:39:16 +0000

    Update Arit.lua

--- tests/Staging/Arit.lua
+++ tests/Staging/Arit.lua
@@ -1,28 +1,28 @@
 local a = math.floor(12)
 local b = math.floor(15)
 local c = 1 + b
-print(c)
+assert(c == 16)
 local c = 1 - b
-print(c)
+assert(c == -14)
 local c = 1 / b
-print(c)
+assert(c == 0.06666666666666667)
 local c = 1 * b
-print(c)
+assert(c == 15)
 local c = 1 % b
-print(c)
+assert(c == 1)
 local c = 1 ^ b
-print(c)
+assert(c == 1)
 local c = a + b
-print(c)
+assert(c == 27)
 local c = a - b
-print(c)
+assert(c == -3)
 local c = a / b
-print(c)
+assert(c == 0.8)
 local c = a * b
-print(c)
+assert(c == 180)
 local c = a % b
-print(c)
+assert(c == 12)
 local c = a ^ b
-print(c)
+assert(c == 15407021574586368)
 
 OK()

947977a769b41fe009fe0e624e9acb1201a372bf
James <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2024-03-27 20:27:17 +0000

    Update Closure.lua

--- tests/Staging/Closure.lua
+++ tests/Staging/Closure.lua
@@ -1,7 +1,8 @@
-print((function() return 5 end)())
+assert((function() return 5 end)() == 5)
 
 local upvalue = math.floor(1)
 
-print((function() upvalue = math.floor(5) return upvalue end)())
+assert((function() assert(upvalue == 1) upvalue = math.floor(5) assert(upvalue == 5) return upvalue end)() == 5)
+assert(upvalue == 5)
 
 OK()

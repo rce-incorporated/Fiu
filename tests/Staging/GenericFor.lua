4f98b765a4be21ed6665d5a9507ab87fdfe82e38
James <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2024-03-27 20:17:47 +0000

    Update GenericFor.lua

--- tests/Staging/GenericFor.lua
+++ tests/Staging/GenericFor.lua
@@ -1,11 +1,23 @@
+local t = {}
+
 for i,v in {1,2,3} do 
-	print(i,v)
+	t[i] = v
 end 
+
+assert(t[1] == 1)
+assert(t[2] == 2)
+assert(t[3] == 3)
 
 print("GENERALIZED DONE")
 
+local t2 = {}
 for i,v in pairs({6,7,8,9}) do 
- 	print(i,v)
+ 	t2[i] = v
 end
+
+assert(t2[1] == 6)
+assert(t2[2] == 7)
+assert(t2[3] == 8)
+assert(t2[4] == 9)
 
 OK()

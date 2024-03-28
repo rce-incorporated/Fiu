5b62ef587890c5a383ba743808dbb3444e65c3d0
James <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2024-03-28 00:18:15 +0000

    Update print.lua

--- examples/print.lua
+++ examples/print.lua
@@ -2,5 +2,5 @@
 
 local Bytecode = "\5\1\2\5\112\114\105\110\116\12\72\101\108\108\111\32\87\111\114\108\100\33\1\2\0\0\1\2\0\6\65\0\0\0\12\0\1\0\0\0\0\64\5\1\2\0\21\0\2\1\22\0\1\0\3\3\1\4\0\0\0\64\3\2\0\1\0\1\24\0\0\0\0\0\0\1\0\0\0\0\0"
 
-local luau_execute = luau_load(Bytecode, { print = print })
+local luau_execute = Fiu.luau_load(Bytecode, { print = print })
 luau_execute()

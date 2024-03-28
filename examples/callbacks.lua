f53f3fc2c5fbf36dd149942b89e32119fc7dcf0e
James <85808999+TheGreatSageEqualToHeaven@users.noreply.github.com>
2024-03-28 00:17:55 +0000

    Update callbacks.lua

--- examples/callbacks.lua
+++ examples/callbacks.lua
@@ -2,14 +2,14 @@
 
 local Bytecode = "\5\1\1\5\109\97\99\114\111\1\1\0\0\1\2\0\5\65\0\0\0\12\0\1\0\0\0\0\64\21\0\1\1\22\0\1\0\2\3\1\4\0\0\0\64\0\1\0\1\24\0\0\0\0\1\1\0\0\0\0\0"
 
-local luau_settings = luau_newsettings()
+local luau_settings = Fiu.luau_newsettings()
 
 luau_settings.callHooks.interruptHook = function(stack, debugging, proto, module, upvals)
 	print("Interrupted: ", debugging.name)
 	print("Previous Instruction: ", proto.code[debugging.pc-2].opname)
 	print("Next Instruction: ", proto.code[debugging.pc+1].opname)
 	
-	stack[proto.code[debugging.pc+1].A] = function() error("This function was placed by the interrupt hook!") end
+	stack[proto.code[debugging.pc].A] = function() error("This function was placed by the interrupt hook!") end
 end
 
 luau_settings.callHooks.stepHook = function(stack, debugging, proto, module, upvals)
@@ -20,5 +20,5 @@ luau_settings.callHooks.panicHook = function(message, stack, debugging, proto, m
 	print("VM Panic: ", message)
 end
 
-local luau_execute = luau_load(Bytecode, {}, luau_settings)
+local luau_execute = Fiu.luau_load(Bytecode, {}, luau_settings)
 luau_execute()

2a781b87229af2ca444470f414b9d24f65fad084
Snorlax <57375992+SnorlaxAssist@users.noreply.github.com>
2024-05-22 11:57:00 -0600

    Update Luau from `0.625` to `0.626` + `fiu-test` update (#38)
    - Adds flag-based test cases.

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 #include "luau/CLI/Coverage.h"
 #include "Luau/Compiler.h"
 #include "Luau/CodeGen.h"
+#include "Luau/ExperimentalFlags.h"
 
 #include "Config.h"
 
@@ -70,6 +71,12 @@ static Luau::CompileOptions compleOptions()
 
 string fiuBytecode;
 bool fiuSupportsCodeGen = false;
+bool fiuRunSupported = false;
+const char* const fiuUnsupportedFlags[] = {
+	"LuauFastCrossTableMove",
+	"LuauCompileTypeInfo",
+	"LuauCompileNoJumpLineRetarget",
+};
 
 TestCompileResult compileTest(string source, Luau::CompileOptions opts, const char* name = "Compiled", bool codegen = false)
 {
@@ -247,6 +254,28 @@ static int lua_collectgarbage(lua_State* L)
 	}
 
 	luaL_error(L, "collectgarbage must be called with 'count' or 'collect'");
+}
+
+bool isFiuSupported(const char* flag)
+{
+	if (!fiuRunSupported)
+		return true;
+	for (const char* item : fiuUnsupportedFlags) {
+		if (item && strcmp(item, flag) == 0)
+			return false;
+	}
+
+	return true;
+}
+
+// Ref: https://github.com/luau-lang/luau/blob/c73ecd8e08c488acd22db9f04c8935471d170e37/CLI/Flags.cpp#L31-L36
+void setLuauFlagsDefault()
+{
+	for (Luau::FValue<bool>* flag = Luau::FValue<bool>::list; flag; flag = flag->next)
+		if (strncmp(flag->name, "Luau", 4) == 0 && !Luau::isFlagExperimental(flag->name) && isFiuSupported(flag->name)) {
+			flag->value = true;
+			printf("[%s] Flag '%s' set to true\n", SUCCESS_SYMBOL, flag->name);
+		}
 }
 
 int testOk(lua_State* L)
@@ -845,6 +874,14 @@ int main(int argc, char* argv[])
 			i++;
 			const char* vectorType = argv[i];
 			globalOptions.vectorType = vectorType;
+		}
+		else if (strcmp(argv[i], "-defaultflags") == 0)
+		{
+			setLuauFlagsDefault();
+		}
+		else if (strcmp(argv[i], "-fiureadyflags") == 0)
+		{
+			fiuRunSupported = true;
 		}
 		else
 		{
